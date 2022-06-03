f5fea0fa2555d8a883d7bf7ff54104ef0b6a62cc
jungsiroo <fordevelop@pm.me>
2022-06-03 15:27:54 +0000

    Refactor : Calculate Metric in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse, FileResponse
 import uvicorn
@@ -8,12 +9,13 @@ from transformers import AutoTokenizer, AutoModelForTokenClassification
 import torch
 from find_job_titles import FinderAcora
 from util import log_and_config
+from calculate.metric import *
+from calculate.pickle import *
 import cv2
-
+import argparse
 
 class Image_str(BaseModel):
     image_str: str
-
 
 app = FastAPI()
 
@@ -49,11 +51,31 @@ def image_upload():
 def image_upload():
     return FileResponse("temp.jpg")
 
+def run(args):
+    if args.calculate:
+        if not os.path.exists("./pred.pkl"):
+            save_pred_pickle(callback_fn=main, model=model, tokenizer=tokenizer, device=device, finder=finder)
+
+        pred = load_pred_pickle()
+
+        true = make_true_list("./data_info.csv")
+
+        print(f"CAL ACC : {calculate_accuracy(true, pred)}")
+
+    else:
+        # uvicorn.run(app, host="0.0.0.0", port=30001)
+        img = cv2.imread("/opt/ml/final/data/test_dataset/raw_image/raw_image16.jpg")
+        img = cv2.imencode(".PNG", img)[1].tobytes()
+        print(
+            main(img=img, model=model, tokenizer=tokenizer, device=device, finder=finder)
+        )
 
 if __name__ == "__main__":
-    # uvicorn.run(app, host="0.0.0.0", port=30001)
-    img = cv2.imread("/opt/ml/final/data/image/image64.jpg")
-    img = cv2.imencode(".PNG", img)[1].tobytes()
-    print(
-        main(img=img, model=model, tokenizer=tokenizer, device=device, finder=finder)
-    )
+    parser = argparse.ArgumentParser()
+
+    # Metric 측정용 Argument
+    parser.add_argument('--calculate', type=bool, default=False,
+                        help='Calculate Accuracy')
+
+    args= parser.parse_args()
+    run(args)
